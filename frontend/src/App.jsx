0dda652223e9a2321bbdbc6d6b5cc0cc09e0727f
Do Bruker <dorbruker1@gmail.com>
2024-02-28 08:50:03 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <div className="card">
+        <div>
+          hello world
+        </div>
+      </div>
+      <p className="read-the-docs">
+        maybe write some code and then enter...
+      </p>
+    </>
+  )
+}
+
+export default App
