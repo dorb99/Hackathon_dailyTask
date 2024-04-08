9166beceb9402f6cc10c7410e2624161f4ba9805
Do Bruker <dorbruker1@gmail.com>
2024-04-08 18:08:03 +0300

    aaa

--- backend/routes/roomRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const roomController = require("../controllers/roomController");
-
-router.route("/create").post(roomController.createRoom);
-// router.route("/deleteRoom").delete(roomController.findAndDeleteRoom);
-router.route("/getRoom").patch(roomController.getRoom);
-
-module.exports = router;
